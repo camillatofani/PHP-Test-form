16df41fce30b706903151be15464859718c59da7
Camilla Tofani <79524924+camillatofani@users.noreply.github.com>
2021-11-16 14:02:06 +0100

    Update!
    Division of numbers from non-numbers.
    Decimal rounding.

--- order.php
+++ order.php
@@ -4,19 +4,28 @@
 
 $numbers = array_values($_POST);
 $array_length = count($numbers);
-echo "<h1>"."Before Sort"."</h1>";
 
-for($i=0;$i<$array_length;$i++)
-{
-echo $numbers[$i] ."<br>";
-}
+    echo "<h1>"."Before Sort"."</h1>";
 
-sort($numbers);
-echo "<h1>"."After Sort"."</h1>";
-for($i=0;$i<$array_length;$i++)
-{
-echo $numbers[$i]."<br>";
-}
+    for($i=0;$i<$array_length;$i++)
+        {
+            echo $numbers[$i]."<br>";
+        }
+
+    sort($numbers);
+
+    echo "<h1>"."After Sort"."</h1>";
+
+    for($i=0;$i<$array_length;$i++)
+        {
+            if (is_numeric($numbers[$i])) {
+                echo "A number"."<br>";
+                echo (intval( $numbers[$i] * 100 ) / 100)."<br>";
+            } else {
+                echo "Not a number"."<br>";
+                echo $numbers[$i]."<br>";
+            }
+        }
 
 echo '<br>';
 
